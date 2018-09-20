dcb88a072899ea51eeda061ad7aeece23881e1a0
David masterson <david73180@gmail.com>
2018-09-20 03:35:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
- x = array.join
- return x
+ case array
+   when 0 
+     x = array.join
+     return x
+   when 2 
+     x = array.join
+     x.split("and")
+     return x
+end
 end
