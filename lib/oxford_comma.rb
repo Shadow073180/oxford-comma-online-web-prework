ac6dc61580ee6f878b731210cf5f85d762682804
David masterson <david73180@gmail.com>
2018-09-20 03:40:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,8 @@ def oxford_comma(array)
      x = array.join
      return x
    when 2 
-     array2 = []
-     array2[0] = array
-     array2[0].split("and")
-     return array2[0]
+      x = array.join
+      x.split("and")
+     return x
 end
 end
