1b6a03d4e45545d89bb64fbe5e6cfa182d66a088
David masterson <david73180@gmail.com>
2018-09-20 04:50:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ def oxford_comma(array)
      x.insert(14, "and ")
       return x
     else 
-     array.insert(last, 0, "and ")
+     array.insert(array.last, 0, "and ")
      return array
     end
 
