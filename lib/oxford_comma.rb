6b8ebf871e203134ffd0629bf8a2a9365b9bc16b
David masterson <david73180@gmail.com>
2018-09-20 04:00:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.length == 1
      x = array.join
-  if x.length == 2
-     x.split(",")
-      puts x
-     end
      return x
+  elsif array.length == 2
+     x = array.join
+     x.split(",")
+     return x
+     end
+    
 end
